df2d9192e53fccd3bbe9143a511aa8ea79d9ccf3
Erdem Redzepi <erdem.r@gmail.com>
2023-03-16 08:59:40 +0100

    NC-400 - Made initial project setup

---
+++ src/pages/privacy.js
@@ -0,0 +1,9 @@
+import styles from "@/styles/Privacy.module.css";
+
+export default function Privacy() {
+  return (
+    <main className={styles.main}>
+      <h1>Privacy</h1>
+    </main>
+  );
+}
