6da0f5a89718c5070c63e977396eaf93fa96a4ce
bayunugrohoDev <bayunugroho963@gmail.com>
2023-12-13 18:14:24 +0700

    NC-345 Implement integration FB/Meta Pixel

--- src/core/constants/config.js
+++ src/core/constants/config.js
@@ -2,5 +2,8 @@ export const config = {
     googleAnalytics : {
         MEASUREMENT_ID : process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID
     },
+    facebookPixel : {
+        FACEBOOK_PIXEL_ID : process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID
+    },
     environment: process.env.APP_ENV,
 }
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,28 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
 import Layout from "@/components/Layout";
 
 import "@/styles/globals.css";
+import { config } from "@/core/constants/config";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    import('react-facebook-pixel')
+      .then((x) => x.default)
+      .then((ReactPixel) => {
+        ReactPixel.init(config.facebookPixel.FACEBOOK_PIXEL_ID) // facebookPixelId
+        ReactPixel.pageView()
+
+        router.events.on('routeChangeComplete', () => {
+          ReactPixel.pageView()
+        })
+      })
+      
+  }, [router.events])
+
   return (
     <Layout>
       <Component {...pageProps} />
