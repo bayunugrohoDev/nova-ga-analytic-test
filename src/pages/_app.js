866f0617b0f4ad1efe4e9975d082a45676f8e8f3
bayunugrohoDev <bayunugroho963@gmail.com>
2024-01-16 17:53:29 +0700

    NC-398 install googgle tag manager lib with real GTM ID

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -26,7 +26,7 @@ export default function App({ Component, pageProps }) {
 
   return  (
     <Layout>
-      <GoogleTagManager gtmId={config.googleAnalytics.MEASUREMENT_ID} />
+      <GoogleTagManager gtmId={config.googleTagManager.GTM_ID} />
       <Component {...pageProps} />
     </Layout>
   );
